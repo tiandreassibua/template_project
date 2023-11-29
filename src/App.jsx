311434a50ba30f3e2f47d39c27a5537b39c09081
Andreas Sibua <andreas.sibua@ti.ukdw.ac.id>
2023-11-30 06:54:46 +0700

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+const App = () => {
+    return (
+        <div className="min-h-screen flex justify-center items-center bg-neutral-800">
+            <h1 className="text-8xl font-bold text-slate-500">Hello world</h1>
+        </div>
+    );
+};
+
+export default App;
